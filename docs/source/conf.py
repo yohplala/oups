cc5d3c7e129853291340a0e14ac66cae947478fc
yohplala <pierre.juillard@gmail.com>
2022-01-05 21:12:25 +0100

    Modif in config.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -9,32 +9,51 @@
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
-#
+
 import os
+import sys
 
 
-# sys.path.insert(0, os.path.abspath('.'))
-
+sys.path.insert(0, os.path.abspath("."))
 
 # -- Project information -----------------------------------------------------
 
 project = "oups"
 copyright = "2022, Yohplala"
 author = "Yohplala"
-
-# The full version, including alpha/beta/rc tags
-release = "0.1.0"
-
+version = "latest"
+release = version
 
 # -- General configuration ---------------------------------------------------
 
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = []
+extensions = [
+    "sphinx.ext.autodoc",
+    "sphinx.ext.napoleon",
+    "numpydoc",
+]
 
-# Add any paths that contain templates here, relative to this directory.
-templates_path = ["_templates"]
+# The master toctree document
+master_doc = "index"
+
+# The name of the Pygments (syntax highlighting) style to use
+pygments_style = "friendly"
+
+# Don't auto-generate summary for class members
+numpydoc_show_class_members = False
+
+# Do not prepend module name to functions
+add_module_names = False
+todo_include_todos = False
+
+autosummary_generate = True
+autodoc_member_order = "bysource"
+
+napoleon_google_docstring = False
+# Do not show the return type as separate section
+napoleon_use_rtype = False
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
@@ -47,16 +66,8 @@ exclude_patterns = []
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 # html_theme = 'alabaster'
+html_theme = "sphinx_rtd_theme"
 
-# Taken from docs.readthedocs.io:
-# on_rtd is whether we are on readthedocs.io
-on_rtd = os.environ.get("READTHEDOCS", None) == "True"
-
-if not on_rtd:  # only import and set the theme if we're building docs locally
-    import sphinx_rtd_theme
-
-    html_theme = "sphinx_rtd_theme"
-    html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
